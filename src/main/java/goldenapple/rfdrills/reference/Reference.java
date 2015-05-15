ab95af01f8e1b4e38310fb48d12a2728db38f04e
goldenapple3 <goldenappleminecraft@gmail.com>
2015-05-15 17:08:44 +0300

    1.3 update + add Redstone Arsenal dependency

--- src/main/java/goldenapple/rfdrills/reference/Reference.java
+++ src/main/java/goldenapple/rfdrills/reference/Reference.java
@@ -3,8 +3,8 @@ package goldenapple.rfdrills.reference;
 public class Reference {
     public static final String MOD_ID = "rfdrills";
     public static final String MOD_NAME = "RFDrills";
-    public static final String VERSION = "1.2.1";
-    public static final String DEPENDECIES = "required-after:CoFHLib;after:ThermalExpansion;after:EnderIO";
+    public static final String VERSION = "1.3";
+    public static final String DEPENDECIES = "required-after:CoFHLib; after:ThermalExpansion; after:EnderIO; after:RedstoneArsenal";
 
     public static final String GUI_FACTORY = "goldenapple.rfdrills.gui.RFDrillsGuiFactory";
 }
