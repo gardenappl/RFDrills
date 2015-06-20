298592db83b2a2df1df80e5d40e8ae2abd2b8555
goldenapple3 <goldenappleminecraft@gmail.com>
2015-06-20 17:19:06 +0300

    1.4.3 update!

--- src/main/java/goldenapple/rfdrills/reference/Reference.java
+++ src/main/java/goldenapple/rfdrills/reference/Reference.java
@@ -3,7 +3,7 @@ package goldenapple.rfdrills.reference;
 public class Reference {
     public static final String MOD_ID = "rfdrills";
     public static final String MOD_NAME = "RFDrills";
-    public static final String VERSION = "1.4.2";
+    public static final String VERSION = "1.4.3";
     public static final String DEPENDECIES = "required-after:CoFHCore; after:ThermalExpansion; after:EnderIO; after:RedstoneArsenal";
 
     public static final String GUI_FACTORY = "goldenapple.rfdrills.gui.RFDrillsGuiFactory";
