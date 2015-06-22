e628011580ee28b3b467d91f32480b1619f82752
goldenapple3 <goldenappleminecraft@gmail.com>
2015-06-22 16:49:17 +0300

    More TO-DO

--- src/main/java/goldenapple/rfdrills/config/EnumEnergyUnit.java
+++ src/main/java/goldenapple/rfdrills/config/EnumEnergyUnit.java
@@ -4,7 +4,7 @@ public enum EnumEnergyUnit { //i don't want energy conversion so this is pretty 
     RF(1.0F),
     EU(0.25F), //4 RF = 1 EU
     J(2.5F), //1 RF = 2.5 J
-    gJ(1.6F); //1 RF = 1.6 GalactiCraft J
+    gJ(1.6F); //1 RF = 1.6 GalactiCraft J TODO: GalactiCraft support
 
     private float conversionRateFromRF;
 
--- src/main/java/goldenapple/rfdrills/item/soulupgrade/SoulUpgradeHelper.java
+++ src/main/java/goldenapple/rfdrills/item/soulupgrade/SoulUpgradeHelper.java
@@ -2,8 +2,12 @@ package goldenapple.rfdrills.item.soulupgrade;
 
 import net.minecraft.item.ItemStack;
 
-public class SoulUpgradeHelper {
+public class SoulUpgradeHelper {//TODO
     public static void applyUpgrade(ItemStack itemStack, AbstractSoulUpgrade upgrade, int level){
-        //TODO
+
+    }
+
+    public static int getUpgradeLevel(ItemStack itemStack, AbstractSoulUpgrade upgrade){
+
     }
 }
