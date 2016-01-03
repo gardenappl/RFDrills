9f0b9fcc76c97848aef1fbc278eb79dbdf8dadbd
goldenapple3 <goldenappleminecraft@gmail.com>
2016-01-03 18:36:38 +0200

    1.7.3 update!

--- src/main/java/goldenapple/rfdrills/reference/Reference.java
+++ src/main/java/goldenapple/rfdrills/reference/Reference.java
@@ -3,7 +3,7 @@ package goldenapple.rfdrills.reference;
 public class Reference {
     public static final String MOD_ID = "rfdrills";
     public static final String MOD_NAME = "RFDrills";
-    public static final String VERSION = "1.7.2";
+    public static final String VERSION = "1.7.3";
     public static final String DEPENDENCIES = "required-after:CoFHCore; after:ThermalExpansion; after:EnderIO; after:RedstoneArsenal; after:simplyjetpacks";
 
     public static final String COMMON_PROXY = "goldenapple.rfdrills.CommonProxy";
